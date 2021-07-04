f5a5807d8690f1228e066319d3eeca12ba27af6c
Victor Buch-Hansen <victorbuchhansen@gmail.com>
2021-07-04 19:18:32 +0200

    Added docker support

--- app.py
+++ app.py
@@ -8,49 +8,47 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+
+class item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ip = db.Column(db.String(100))
-    mac = db.Column(db.String(100))
+    title = db.Column(db.String(50))
+    mac = db.Column(db.String(30))
+
 
 @app.route("/")
-def home():
-    todo_list = Todo.query.all()
-    return render_template("base.html", todo_list=todo_list)
+def index():
+    item_list = item.query.all()
+    return render_template("base.html", item_list=item_list)
 
 
 @app.route("/add", methods=["POST"])
 def add():
-    ip = request.form.get("ip")
+    title = request.form.get("title")
     mac = request.form.get("mac")
-    new_item = Todo(ip=ip, mac=mac)
+    new_item = item(title=title, mac=mac)
     db.session.add(new_item)
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("index"))
 
 
-@app.route("/update/<int:todo_id>")
-def update(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    db.session.commit()
-    return redirect(url_for("home"))
+@app.route("/wake/<int:item_id>")
+def wake(item_id):
+    wol = item.query.filter_by(id=item_id).first()
 
-@app.route("/wake/<int:todo_id>")
-def wake(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-
-    send_magic_packet(todo.mac, ip_address=todo.ip, port=9)
+    send_magic_packet(wol.mac)
 
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("index"))
 
-@app.route("/delete/<int:todo_id>")
-def delete(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    db.session.delete(todo)
+
+@app.route("/delete/<int:item_id>")
+def delete(item_id):
+    wol = item.query.filter_by(id=item_id).first()
+    db.session.delete(wol)
     db.session.commit()
-    return redirect(url_for("home"))
+    return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
